99b6c7205659f48074152462a265a4231e746056
Smit075 <shahsmit075@gmail.com>
2024-07-07 23:47:48 +0530

    first commit

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
+
+
+export default function Home() {
+  return (
+    <div className="test-2xl">
+      jknfgerlbger
+    </div>
+  );
+}
